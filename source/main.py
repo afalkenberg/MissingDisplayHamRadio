2b1b1a3447181c1b2d4269ee852a746392883e10
afalkenberg <andreas@falkenbergtech.com>
2025-02-19 11:15:31 -0800

    just parameter

--- source/main.py
+++ source/main.py
@@ -2,7 +2,7 @@ import VolumePlot
 import ReceiverAudioFilter
 
 # VP = VolumePlot.VolumePlot(True)
-VP = ReceiverAudioFilter.ReceiverAudioFilter('ssb', True)
+VP = ReceiverAudioFilter.ReceiverAudioFilter('data', False)
 
 VP.createMainWindow()
 
